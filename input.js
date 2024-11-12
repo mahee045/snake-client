3177fbdc15a18d11587f6f00cfc25718e883b869
Maheerah Anwar <maheerahanwar@gmail.com>
2024-11-11 20:03:48 -0500

    WASD movement

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -12,6 +16,26 @@ const handleUserInput = function (key) {
   // Exit the game when 'Ctrl + C' is pressed
   if (key === '\u0003') {
     process.exit();
+  }
+
+  // Move up when 'w' is pressed
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  
+  // Move down when 's' is pressed
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  
+  // Move left when 'a' is pressed
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  
+  // Move right when 'd' is pressed
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
