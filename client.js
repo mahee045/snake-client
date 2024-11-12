a4edf25b6e7ff65d0ffcf3092fe5724d664c0ad1
Maheerah Anwar <maheerahanwar@gmail.com>
2024-11-11 20:34:45 -0500

    final cleanup

--- client.js
+++ client.js
@@ -22,10 +22,8 @@ const connect = function () {
   conn.on('connect', () =>{
     console.log('Successfully connected to game server')
     ///Name of snake
-    conn.write('Name:MAH'); 
-     // Commenting out the hard-coded move message
-    // send move up command to server
-   // conn.write('Move: up');
+    conn.write("Name: MAH"); 
+
    
   })
 
